9d2e88605d25e75dd17a366c51065c2b071d0d37
Poly2it <84731064+Poly2it@users.noreply.github.com>
2024-02-03 19:22:50 +0100

    Patch multiple issues with dependency manager

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -29,10 +29,10 @@ case "$(uname -o)" in
         exit 0
         ;;
     "MS/Windows")
-        source "${PROGRAM_WORKING_DIRECTORY}/scripts/lib_w64devkit.sh"
+        . "${PROGRAM_WORKING_DIRECTORY}/scripts/lib_w64devkit.sh"
         ;;
-    "Linux")
-        source "${PROGRAM_WORKING_DIRECTORY}/scripts/lib_linux.sh"
+    "Linux" | "GNU/Linux")
+        . "${PROGRAM_WORKING_DIRECTORY}/scripts/lib_linux.sh"
         ;;
     *)
         break
@@ -44,7 +44,7 @@ main() {
     local DEPENDENCY="$1"
     local SUBCOMMAND="$2"
     cd "${PROGRAM_WORKING_DIRECTORY}/resources/external"
-    "${DEPENDENCY}_${SUBCOMMAND}" 1>/dev/null 2>&1 || printf "invalid dependency or subcommand: \"%s\" \"%s\"\n" "${DEPENDENCY}" "${SUBCOMMAND}"
+    "${DEPENDENCY}_${SUBCOMMAND}" || printf "invalid dependency or subcommand: \"%s\" \"%s\"\n" "${DEPENDENCY}" "${SUBCOMMAND}"
 }
 
 
@@ -112,10 +112,13 @@ raylib_install() {
     (
         cd "raylib/src"
 
+        mkdir --parents "$(path_local_share_get)/lib"
+        mkdir --parents "$(path_local_share_get)/include"
+
         cp --update --verbose libraylib.a "$(path_local_share_get)/lib/libraylib.a"
-        cp --update raylib.h "$(path_local_share_get)/include/raylib.h"
-        cp --update raymath.h "$(path_local_share_get)/include/raymath.h"
-        cp --update rlgl.h "$(path_local_share_get)/include/rlgl.h"
+        cp --update --verbose raylib.h "$(path_local_share_get)/include/raylib.h"
+        cp --update --verbose raymath.h "$(path_local_share_get)/include/raymath.h"
+        cp --update --verbose rlgl.h "$(path_local_share_get)/include/rlgl.h"
     )
 }
 
@@ -134,10 +137,10 @@ raylib_uinstall() {
 raylib_all() {
     local HOST="https://github.com/raysan5/raylib.git"
     local PATH_TARGET="raylib"
-    local HASH_REQUIRED="deaffb06981b95976f7b055dc1085adb933a37cc"
+    local HASH_REQUIRED="29ff658d9223068378269e4a705811f085fafdf4"
 
     if [ ! -d "${PATH_TARGET}" ]; then
-        git clone "${HOST}" "${PATH_TARGET}" --depth=1 2>&1 1>&3 || exit 1
+        git clone "${HOST}" "${PATH_TARGET}" 2>&1 1>&3 || exit 1
 
         HASH_LOCAL="$( \
             git log \
@@ -153,7 +156,10 @@ raylib_all() {
             cd "raylib/src"
             make -j$(nproc) 1>&3 2>&4
 
-            cp --update --verbose libraylib.a "$(path_local_share_get)/lib/libraylib.a"
+            mkdir --parents "$(path_local_share_get)/lib"
+            mkdir --parents "$(path_local_share_get)/include"
+
+            cp --update libraylib.a "$(path_local_share_get)/lib/libraylib.a"
             cp --update raylib.h "$(path_local_share_get)/include/raylib.h"
             cp --update raymath.h "$(path_local_share_get)/include/raymath.h"
             cp --update rlgl.h "$(path_local_share_get)/include/rlgl.h"
@@ -172,14 +178,22 @@ raylib_all() {
                 echo "${PATH_TARGET}: Up to date"
             else
                 echo "${PATH_TARGET}: Updating"
-                git pull 2>&1 1>&3 || exit 1
+                git fetch 2>&1 1>&3 || exit 1
                 git checkout "${HASH_REQUIRED}" 2>&1 1>&3 || exit 1
+
                 make -j$(nproc) 1>&3 2>&4
 
-                cp --update --verbose libraylib.a "$(path_local_share_get)/lib/libraylib.a"
-                cp --update raylib.h "$(path_local_share_get)/include/raylib.h"
-                cp --update raymath.h "$(path_local_share_get)/include/raymath.h"
-                cp --update rlgl.h "$(path_local_share_get)/include/rlgl.h"
+                (
+                    cd "src"
+                    
+                    mkdir --parents "$(path_local_share_get)/lib"
+                    mkdir --parents "$(path_local_share_get)/include"
+
+                    cp --update --verbose libraylib.a "$(path_local_share_get)/lib/libraylib.a"
+                    cp --update --verbose raylib.h "$(path_local_share_get)/include/raylib.h"
+                    cp --update --verbose raymath.h "$(path_local_share_get)/include/raymath.h"
+                    cp --update --verbose rlgl.h "$(path_local_share_get)/include/rlgl.h"
+                )
                 echo "${PATH_TARGET}: Finished updating, up to date"
             fi
         )
